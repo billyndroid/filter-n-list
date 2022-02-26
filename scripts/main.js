923502362b6284acdb109f2e386aec4d9b9c4a9e
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-02-26 18:30:47 +0000

    update
    bugfix

--- scripts/main.js
+++ scripts/main.js
@@ -14,9 +14,10 @@ function filterNames() {
     //loop through listItem lis
     for(let i = 0; i < li.length; i++) {
         let a = li[i].getElementsByTagName("a")[0];
-        //if matched
+        //if matched 
         if(a.innerHTML.charAt(0).toUpperCase().indexOf(filterValue) > -1) {
             li[i].style.display = "";
+            //if first letter is not matched the list will filter out results - need to apply this to all other letters
         } else {
             li[i].style.display = "none";
         }
